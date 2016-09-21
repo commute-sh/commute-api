d6e6ebd88e3bf6ea13d7e3fc0823b3e060a81e6a
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-09-21 22:39:08 +0200

    Added station fetch by ids

--- index.js
+++ index.js
@@ -6,15 +6,35 @@ const moment = require('moment');
 
 app.use(requestLogger());
 
+app.get('/stations', function (req, res) {
+    const start = moment();
+
+    const contractName = req.query.contract_name || 'Paris';
+    const ids = (req.query.ids || "").split(',');
+
+    stationService.findByIds(contractName, ids).then((stations) => {
+        res.status(200).json(stations);
+
+        const duration = moment.duration(moment().diff(start)).milliseconds();
+
+        console.log("*** Station provided in", duration, "ms");
+
+    }).catch((err) => {
+        console.log('[ERROR]', err.message, ' - stack:', err.stack);
+        res.status(500).json({ message: err.message });
+    });
+
+});
+
 app.get('/stations/nearby', function (req, res) {
     const start = moment();
 
-    const city = 'Paris';
+    const contractName = req.query.contract_name || 'Paris';
     const lat = Number(req.query.lat);
     const lng = Number(req.query.lng);
     const distance = Number(req.query.distance || '5000');
 
-    stationService.nearby(city, lat, lng, distance).then((stations) => {
+    stationService.nearby(contractName, lat, lng, distance).then((stations) => {
         res.status(200).json(stations);
 
         const duration = moment.duration(moment().diff(start)).milliseconds();
@@ -52,5 +72,5 @@ app.get('/stations/:contractName/:stationNumber/:date/data', function (req, res)
 var httpPort = process.env.HTTP_PORT || 3000;
 
 app.listen(httpPort, function () {
-    console.log('Commute API listening on port 3000!');
+    console.log(`Commute API listening on port ${httpPort} !`);
 });
