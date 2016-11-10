ff11b5fe3a034ea6cd959637d3a56945fd13f3a4
Alexis Kinsella <alexis.kinsella@gmail.com>
2016-11-11 00:56:37 +0100

    Added a dummy station related test

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const request = require('request');
+const sinon = require('sinon');
+const chai = require('chai');
+const fs = require('fs');
+const expect = require('chai').expect;
+
+describe('Stations', () => {
+
+    it('Should Find stations nearby', (done) => {
+        done();
+    });
+
+});
